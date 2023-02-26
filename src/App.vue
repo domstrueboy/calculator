96c8592ecb787db322532a664827f8453588da5b
Pavel Bobylyov <p.bobylyov@movavi.com>
2023-02-26 13:19:15 +0500

    some styles

--- src/App.vue
+++ src/App.vue
@@ -3,40 +3,33 @@
 </script>
 
 <template>
-  <header>
-    Hello
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="overlay">
+    <main class="calculator">
+      lkasjdlfk
+      <DisplayArea />
+      <KeyboardArea />
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.overlay {
+  background: var(--white);
+  opacity: 0.2;
+  backdrop-filter: blur(2px);
+  border-radius: 18px;
+  padding: 38px 31px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.calculator {
+  background: var(--main-background);
+  padding: 45px;
+  border-radius: 18px;
+  box-shadow: 0px 82px 158px rgba(0, 0, 0, 0.35), 0px 24.7206px 47.6324px rgba(0, 0, 0, 0.228056), 0px 10.2677px 19.7841px rgba(0, 0, 0, 0.175), 0px 3.71362px 7.1555px rgba(0, 0, 0, 0.121944);
+  z-index: 1000;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
